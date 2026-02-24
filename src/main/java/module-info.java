dc05137d2773cf54a672385bbf6b2e3dbf4e587d
fadi <fadimagdi97@yahoo.com>
2026-02-24 22:03:03 +0200

    estabilishing project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.socialmediaapp.social_media_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.socialmediaapp.social_media_app to javafx.fxml;
+    exports org.socialmediaapp.social_media_app;
+}
---
+++ src/main/java/org/socialmediaapp/social_media_app/HelloController.java
@@ -0,0 +1,14 @@
+package org.socialmediaapp.social_media_app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
